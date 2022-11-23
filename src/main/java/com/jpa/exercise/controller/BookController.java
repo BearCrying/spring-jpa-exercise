5491503b25e4a73bbc3aa26e9eb947e7e1f08f45
DESKTOP-RJC8TSU\taege <djqtek0525@naver.com>
2022-11-23 13:12:35 +0900

    first commit

---
+++ src/main/java/com/jpa/exercise/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.jpa.exercise.controller;
+
+import com.jpa.exercise.domain.Book;
+import com.jpa.exercise.repository.BookRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Page<Book>>
+}
