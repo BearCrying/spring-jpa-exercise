1bd0d2592a8a725bb7b702edebc7ea0810f4271a
DESKTOP-RJC8TSU\taege <djqtek0525@naver.com>
2022-11-23 23:09:25 +0900

    [add] publisher 추가
    조회했을때 책이름,저자,출판사가 함께 나오게

--- src/main/java/com/jpa/exercise/domain/Book.java
+++ src/main/java/com/jpa/exercise/domain/Book.java
@@ -1,19 +1,29 @@
 package com.jpa.exercise.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "book")
 public class Book {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private Long authorId; // id만
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    @OneToOne
+    @JoinColumn(name = "publisher_id")
+    private Publisher publisher;
 }
--- src/main/java/com/jpa/exercise/domain/Publisher.java
+++ src/main/java/com/jpa/exercise/domain/Publisher.java
@@ -1,10 +1,22 @@
 package com.jpa.exercise.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Publisher {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    public String name;
-    public String address;
+    private String name;
+    private String address;
 }
